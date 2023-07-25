a39c03ba1555ed6e39c9635fc14aede1bf3a1fba
mmmosd-games <chlgustj132@gmail.com>
2023-07-25 21:37:53 +0900

    _

--- app.py
+++ app.py
@@ -2,3 +2,12 @@ import streamlit as st
 import model
 
 model.printTFVersion()
+
+st.set_page_config(
+    page_icon="🎹",
+    page_title="mmmosdMusicAI",
+    layout="wide",
+)
+
+st.header("fdas")
+st.subheader("fdsa")
