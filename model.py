ac63e7eeaf06cc34402c4844218859b0e879ef8a
mmmosd-games <chlgustj132@gmail.com>
2023-07-28 17:03:33 +0900

    .

--- model.py
+++ model.py
@@ -9,68 +9,17 @@ from tensorflow.keras.layers import Dense, Activation, LeakyReLU, Conv2D, UpSamp
 def printTFVersion():
     print(tf.__version__)
 
-class DiscriminativeModel(Model):
-    def __init__(self):
-        super(DiscriminativeModel, self).__init__()
-        self.conv2d = Conv2D(64, kernel_size=5, padding='same')
-        self.activation = Activation(LeakyReLU(0.2))
-        self.dropout = Dropout(0.3)
-        self.conv2d1 = Conv2D(128, kernel_size=5, strides=2, padding='same')
-        self.flatten = tf.keras.layers.Flatten()
-        self.dense = Dense(1, activation='sigmoid')
+Generator = tf.keras.Sequential([
+    tf.keras.layers.Input(256,30),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(784, activation='sigmoid')
+])
 
-    def call(self, x):
-        x = self.conv2d(x)
-        x = self.activation(x)
-        x = self.dropout(x)
-        x = self.conv2d1(x)
-        x = self.activation(x)
-        x = self.dropout(x)
-        x = self.flatten(x)
-        x = self.dense(x)
-        return x
+Discriminator = tf.keras.Sequential([
+    tf.keras.layers.Input(784),
+    tf.keras.layers.Dense(256, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
 
-
-class GenerativeModel(Model):
-    def __init__(self):
-        super(GenerativeModel, self).__init__()
-        self.dense = Dense(128*7*7, input_dim=100, activation=LeakyReLU(0.2))
-        self.batch = BatchNormalization()
-        self.reshape = Reshape((7, 7, 128))
-        self.upS = UpSampling2D()
-        self.conv2d = Conv2D(64, kernel_size=5, padding='same')
-        self.activation = Activation(LeakyReLU(0.2))
-        self.conv2d1 = Conv2D(1, kernel_size=5, padding='same', activation='tanh')
-
-    def call(self, x):
-        x = self.dense(x)
-        x = self.batch(x)
-        x = self.reshape(x)
-        x = self.upS(x)
-        x = self.conv2d(x)
-        x = self.batch(x)
-        x = self.activation(x)
-        x = self.conv2d1(x)
-        return x
-
-EPOCHS = 100
-
-modelD = DiscriminativeModel()
-modelG = GenerativeModel()
-
-modelD.compile(loss='binary_crossentropy', optimizer='adam')
-modelG.compile(loss='binary_crossentropy', optimizer='adam')
-
-modelD.trainable = False
-
-#modelD + modelG = modelC
-ginput = tf.keras.layers.Input(shape=(100,))
-doutput = modelD(modelG(ginput))
-modelC = tf.keras.Model(ginput, doutput)
-modelC.compile(loss='binary_crossentropy', optimizer='adam')
-
-modelD.save()
-modelG.save()
-
-def trainGan():
-    x_train
+Doptimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
+Goptimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
