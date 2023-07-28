20a8e7e740159d8647a0257e9e3e539aa539e3e2
mmmosd-games <chlgustj132@gmail.com>
2023-07-28 17:04:32 +0900

    .

--- app.py
+++ app.py
@@ -1,21 +1,5 @@
 import streamlit as st
-import discord
 import model
-
-from discord.ext import commands
-
-app = commands.Bot(command_prefix='/')
- 
-@app.event
-async def on_ready():
-    print('Done')
-    await app.change_presence(status=discord.Status.online, activity=None)
-
-@app.command()
-async def hello(ctx):
-    await ctx.send('Hello I am Bot!')
-    
-app.run('MTEzNDM5NDY0MTUwNTA2NzAxOQ.G3_Ucr.9MCbN3gUiAj8UT9MrowMahpJmb8CiapDFnidUU')
 
 model.printTFVersion()
 
