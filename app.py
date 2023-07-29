abd59aab987483738e042e2447e3fdc97982cdbb
mmmosd-games <chlgustj132@gmail.com>
2023-07-29 23:35:08 +0900

    test audio

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 import streamlit as st
 import model
 import converter
+import os
+import base64
+
+def autoplay_audio(file_path: str):
+    with open(file_path, "rb") as f:
+        data = f.read()
+        b64 = base64.b64encode(data).decode()
+        md = f"""
+            <audio controls autoplay="true">
+            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+            </audio>
+            """
+        st.markdown(
+            md,
+            unsafe_allow_html=True,
+        )
 
 st.set_page_config(
     page_icon="🎹",
@@ -10,3 +26,10 @@ st.set_page_config(
 
 st.header("LOFI-LOOP 🎷")
 st.subheader("made by mmmosd")
+
+spg = converter.Sound_To_Spectrogram('Sphere.mp3', 60)
+
+fileName = 'SphereSPG.wav'
+Audio = converter.Save_Spectrogram_Audio(spg, fileName)
+
+autoplay_audio(fileName)
--- converter.py
+++ converter.py
@@ -1,7 +1,6 @@
 import librosa
 import numpy as np
 import soundfile as sf
-import os
 
 from sklearn.preprocessing import MinMaxScaler
 from PIL import Image
@@ -14,7 +13,9 @@ def Sound_To_Spectrogram(FilePath, duration):
 def Save_Spectrogram_Image(Spectrogram, filename):
     arr = MinMaxScaler().fit_transform(Spectrogram)*255
     Image.fromarray(arr).convert('RGB').save(filename, 'JPEG')
+    return arr
 
 def Save_Spectrogram_Audio(Spectrogram, filename):
     audio = librosa.feature.inverse.mel_to_audio(Spectrogram, sr=44100)
     sf.write(filename, audio, 44100)
+    return audio
