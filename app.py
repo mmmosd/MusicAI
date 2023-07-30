4a68f5669f1451de4eb21f0b039793ff1a1b3127
mmmosd-games <chlgustj132@gmail.com>
2023-07-30 12:54:37 +0900

    test

--- app.py
+++ app.py
@@ -1,22 +1,6 @@
 import streamlit as st
 import model
 import converter
-import os
-import base64
-
-def autoplay_audio(file_path: str):
-    with open(file_path, "rb") as f:
-        data = f.read()
-        b64 = base64.b64encode(data).decode()
-        md = f"""
-            <audio controls autoplay="true">
-            <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
-            </audio>
-            """
-        st.markdown(
-            md,
-            unsafe_allow_html=True,
-        )
 
 st.set_page_config(
     page_icon="🎹",
@@ -32,4 +16,4 @@ spg = converter.Sound_To_Spectrogram('Sphere.mp3', 60)
 fileName = 'SphereSPG.wav'
 Audio = converter.Save_Spectrogram_Audio(spg, fileName)
 
-autoplay_audio(fileName)
+st.audio(Audio, format="audio/wav", start_time=0, sample_rate=44100)
